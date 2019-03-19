ec624b790e258a21194fc029bee6b4ef7381d6b0
Ricardo Romao <rdromao@gmail.com>
2019-03-19 17:33:18 -0400

    Removing unnecessary stylings in favor of Bootstraps

--- components/note.js
+++ components/note.js
@@ -1,26 +1,13 @@
-import { Col } from 'reactstrap'
-
-const noteStyle = {
-    backgroundColor: '#fff',
-    color: '#343434',
-    boxShadow: '0px 4px 16px #aaa',
-    padding: 16,
-    margin: 8,
-    borderRadius: 4
-}
-
-const titleStyle = {
-    marginTop: 4,
-    marginBottom: 24
-}
+import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
 
 const Note = (props) => (
-    <Col md="4">
-        <div style={noteStyle}>
-            <h3 style={titleStyle}>{props.note.title}</h3>
-            <div>{props.note.content}</div>
-        </div>
-    </Col>
+    <Card>
+        <CardBody>
+            <CardTitle>{props.note.title}</CardTitle>
+            <CardText>{props.note.content}</CardText>
+            <Button>Button</Button>
+        </CardBody>
+    </Card>
 )
   
 export default Note
--- components/notesAppLayout.js
+++ components/notesAppLayout.js
@@ -1,5 +1,5 @@
-import Header from './header'
 import Head from 'next/head'
+import { Navbar, NavbarBrand } from 'reactstrap'
 
 const layoutStyle = {
   fontFamily: '"Nunito Sans", sans-serif',
@@ -14,7 +14,9 @@ const Layout = props => (
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
       <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi|Chivo|Nunito+Sans" rel="stylesheet" />
     </Head>
-    <Header />
+    <Navbar>
+        <NavbarBrand href="/">Quadminds Notes Test</NavbarBrand>
+    </Navbar>
     {props.children}
   </div>
 )
